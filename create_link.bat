b818334ded8604422ce3b6525a610ceedd9e3510
Emiliano Firmino <emiliano.firmino@gmail.com>
2014-07-17 19:25:51 -0300

    configure line ending of .bat

--- create_link.bat
+++ create_link.bat
@@ -1,4 +1,3 @@
-mklink /H %HOME%\.vimrc %HOME%\.vim\.vimrc
-mklink /H %HOME%\.gvimrc %HOME%\.vim\.gvimrc
-mklink /J %HOME%\vimfiles %HOME%\.vim
-
+mklink /H "%HOME%\.vimrc" "%HOME%\.vim\.vimrc"
+mklink /H "%HOME%\.gvimrc" "%HOME%\.vim\.gvimrc"
+mklink /J "%HOME%\vimfiles" "%HOME%\.vim"
